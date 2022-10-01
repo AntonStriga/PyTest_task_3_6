401a36536aa0ed8d7bf604ff9c20dcf4583de81e
DESKTOP-8IAJS5U\User <glau_76@mail.ru>
2022-10-01 23:59:24 +0300

    Added task solution

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import time
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_add_to_basket_button(browser):
+    browser.get(link)
+    time.sleep(30)
+
+    add_button = browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form button")
+    assert add_button.is_displayed()
